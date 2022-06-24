38bfdc1ee6eb341671393f4d23134686fa77dbd3
torone89 <torone89>
2022-06-24 18:00:39 +0200

    Email Base finita

--- email/js/script.js
+++ email/js/script.js
@@ -2,20 +2,27 @@
 // VERIFICA COLLEGAMENTO JAVA
 console.log("JS")
 
-const email = ["francescotovani@hotmail.it", "fabrimastro@gmail.com", "enricocampani@gmail.com", "marco-lenci@gmail.com"]
+const email = ["francescotovani@hotmail.it", "fabrimastro@gmail.com", "enricocampani@gmail.com", "marco lenci@gmail.com"]
 console.log(email)
 
 const nome = prompt("Inserisci un email,grz");
 console.log(nome)
 
-let accessibilita
+
 
 // seleziono tutti gli elementi contenuti in email"
 
+let accessibilita = "MI dispiace questa email non valida"
+
+
 for (let i = 0; i < email.length; i++) {
-    accessibilita = "Benvenuto nella tua email, accesso consentito"
-    console.log(email[i])
+    if (nome == email[i]) {
+        accessibilita = "Benvenuto nella tua email, accesso consentito"
+    }
+
 }
-// else { accessibilita = "Mi dispiace email non valida, accesso negato" }
+
+console.log(accessibilita)
+// 
 
 
