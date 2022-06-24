12ce21a5af4bd4d59ceb020034cce1df02768959
torone89 <torone89>
2022-06-24 16:59:33 +0200

    seleziono elementi

--- email/js/script.js
+++ email/js/script.js
@@ -1,3 +1,19 @@
 
 // VERIFICA COLLEGAMENTO JAVA
 console.log("JS")
+
+const email = ["francescotovani@hotmail.it", "fabrimastro@gmail.com", "enricocampani@gmail.com", "marco-lenci@gmail.com"]
+console.log(email)
+
+const nome = prompt("Inserisci un email,grz");
+console.log(nome)
+
+let accessibilita
+
+// seleziono tutti gli elementi contenuti in email"
+
+if (email >= email.length) {
+    accessibilita = "Benvenuto nella tua email, accesso consentito"
+}
+else { accessibilita = "Mi dispiace email non valida, accesso negato" }
+console.log(accessibilita)
